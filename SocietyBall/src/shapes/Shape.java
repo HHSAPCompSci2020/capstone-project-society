0f7211c21d9ff7d1ad6e2497a0810d6b45b4186a
Richard Zhang <rzhang714@student.fuhsd.org>
2021-05-03 11:10:22 -0700

    Started Shape

--- SocietyBall/src/shapes/Shape.java
+++ SocietyBall/src/shapes/Shape.java
@@ -1,5 +1,12 @@
-package shapes;
+package SocietyBall.shapes;
+import java.awt.Color;
+import processing.core.PApplet;
+
 
 public class Shape {
-
+	double x, y;
+	Color c;
+	double length, width;
+	double xVelocity, yVelocity;
+	
 }
