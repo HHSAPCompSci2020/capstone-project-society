9d623715103cbd18fc3336ee9e63d15e01a6dfef
Richard Zhang <rzhang714@student.fuhsd.org>
2021-05-06 10:38:04 -0700

    Added Abstract Methods + constructor

--- SocietyBall/src/shapes/Ball.java
+++ SocietyBall/src/shapes/Ball.java
@@ -2,4 +2,21 @@ package shapes;
 
 public class Ball extends Shape {
 
+	public Ball(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void shift(double xMouse, double yMouse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPointInside(double xInput, double yInput) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
