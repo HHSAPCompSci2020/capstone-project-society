148a7459082ead0c051d96305a00e9afe50b385e
rtsai392 <69985044+rtsai392@users.noreply.github.com>
2021-05-13 11:08:39 -0700

    Added the lines

--- SocietyBall/src/shapes/Obstacle.java
+++ SocietyBall/src/shapes/Obstacle.java
@@ -1,15 +1,27 @@
 package shapes;
 
+import processing.core.PApplet;
+
 public class Obstacle extends Shape {
 
 	public Obstacle(double x, double y) {
 		super(x, y);
+		this.x = x;
+		this.y = y;
+		
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void draw(PApplet surface)
+	{
+		super.draw(surface);
+		surface.line((float)x,(float)y,(float)(x+(float)5*Math.random()),(float)(y+(float)5*Math.random()));
 	}
 
 	@Override
 	public boolean isPointInside(double xInput, double yInput) {
 		// TODO Auto-generated method stub
+
 		return false;
 	}
 
